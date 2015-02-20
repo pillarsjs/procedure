70cad5b89f85fa05280c3b20bc4f4ce304b8a9f9
Bifuer <bifuer@odeux.com>
2015-02-20 10:27:23 +0100

    Extended methodology, better error handling and chained results

--- procedure.js
+++ procedure.js
@@ -2,315 +2,204 @@
 "use strict";
 
 module.exports = Procedure;
-function Procedure(data){
-  this.data = data || {};
+function Procedure(){
   this.jobs = [];
   this.main = new Queue();
-  this.main.data = this.data;
+  this.main.main = true;
+  this.main.procedure = this;
+  //console.log("- Creando procedimiento...");
 }
-  Procedure.prototype.add = function(func){
+  Procedure.prototype.add = function(name,func){
     var procedure = this;
-    var args = Array.prototype.slice.call(arguments).slice(1);
-    procedure.jobs.push({func:func,args:args});
+    var args = Array.prototype.slice.call(arguments);
+    if(typeof name !== "string"){
+        func = name;
+        name = "Job-"+procedure.jobs.length;
+        args = args.slice(1);
+    } else {
+        args = args.slice(2);
+    }
+    //console.log("-- Añadida tarea.",args);
+    procedure.jobs.push({name:name,func:func,args:args});
     return procedure;
   };
-  Procedure.prototype.queue = function(handler){
-    var procedure = this;
-    var queue = new Queue();
-    queue.jobs = procedure.jobs;
-    queue.data = procedure.data;
-    procedure.jobs = [];
-    queue.handler = function(error){
-      if(handler){handler.call(queue,error);}
-      if(error){
-        procedure.main.handler(error);
-      } else {
-        procedure.main.done();
-      }
-    };
-    procedure.main.jobs.push({func:queue.launch.bind(queue)});
-    return procedure;
+  Procedure.prototype.queue = function(){
+    return this.resolve('queue');
   };
-  Procedure.prototype.race = function(handler){
+  Procedure.prototype.race = function(){
+    return this.resolve('race');
+  };
+  Procedure.prototype.resolve = function(mode){
     var procedure = this;
-    var race = new Race();
-    race.jobs = procedure.jobs;
-    race.data = procedure.data;
+    var resolve;
+    resolve = mode === 'race' ? new Race() : new Queue();
+    resolve.jobs = procedure.jobs;
+    //console.log("-- Nueva resolución ("+mode+") de "+resolve.jobs.length+" tareas.");
+    resolve.procedure = procedure;
     procedure.jobs = [];
-    race.handler = function(error){
-      if(handler){handler.call(race,error);}
+    resolve.handler = function(error){
+      var data = Array.prototype.slice.call(arguments);
       if(error){
-        procedure.main.handler(error);
+        //console.log("-- Terminado "+mode+" con error",data);
+        procedure.main.handler.apply(procedure.main,data);
       } else {
-        procedure.main.done();
+        //console.log("-- Terminado "+mode+" sin errores",data);
+        procedure.main.done.apply(procedure.main,[undefined].concat(data));
       }
     };
-    procedure.main.jobs.push({func:race.launch.bind(race)});
+    procedure.main.jobs.push({func:resolve.launch.bind(resolve),args:[]});
     return procedure;
   };
   Procedure.prototype.launch = function(handler){
     var procedure = this;
     procedure.main.handler = handler;
     procedure.main.jobs = procedure.main.jobs.concat(procedure.jobs);
+    //console.log("-- Iniciado procedimiento con "+procedure.main.jobs.length+" tareas.");
     procedure.main.launch();
   };
 
 
 Procedure.Race = Race;
 function Race(){
+  this.handler = undefined;
   this.jobs = [];
-  this.data= {};
-  this.counter = 0;
+  this.results = {};
+  this.errors = [];
 }
   Race.prototype.launch = function(){
-    this.counter = this.jobs.length;
-    for(var i = 0,l=this.jobs.length;i<l;i++){
-      Procedure.Launcher(this,this.jobs[i]);
+    var data = Array.prototype.slice.call(arguments);
+    //console.log("--- Iniciando race...",data);
+    //console.log("-- Elimina el callback sobrante del main Queue");
+    data = data.slice(0,-1);
+    if(this.jobs.length===0){
+      this.handler(undefined,this.results);
+    } else {
+      for(var i = 0,l=this.jobs.length;i<l;i++){
+        var job = this.jobs[i];
+        Procedure.Launcher(this,job.name,job.func,job.args);
+      }
     }
   };
-  Race.prototype.done = function(){
-    if(--this.counter === 0){
-     this.handler();
+  Race.prototype.done = function Done(name,error){
+    //console.log("--- Terminada tarea race",arguments);
+    var data = Array.prototype.slice.call(arguments).slice(1);
+    if(error){
+      this.errors.push(error);
+      data = data.slice(1);
     }
-  };
-  Race.prototype.error = function(error){
-    this.handler(error);
+    this.results[name] = data;
+    if(this.jobs.length===Object.keys(this.results).length){
+      if(this.errors.length>0){
+        this.handler(this.errors);
+      } else {
+        this.handler(undefined,this.results);
+      }
+    }
   };
 
 
 Procedure.Queue = Queue;
 function Queue(){
+  this.handler = undefined;
   this.jobs = [];
-  this.data= {};
 }
   Queue.prototype.launch = function(){
+    var data = Array.prototype.slice.call(arguments);
+    //console.log("--- Iniciando queue...",data);
+    if(!this.main){
+      //console.log("-- Elimina el callback sobrante del main Queue");
+      data = data.slice(0,-1);
+    }
     if(this.jobs.length > 0){
-      Procedure.Launcher(this,this.jobs[0]);
+      var job = this.jobs[0];
+      Procedure.Launcher(this,job.name,job.func,data.concat(job.args));
     } else {
-      this.handler();
+      this.handler.apply(this,data);
     }
   };
-  Queue.prototype.done = function(){
-    this.jobs.shift();
-    this.launch();
-  };
-  Queue.prototype.error = function(error){
-    this.handler(error);
+  Queue.prototype.done = function Done(name,error){
+    //console.log("--- Terminada tarea queue",arguments);
+    var data = Array.prototype.slice.call(arguments).slice(1);
+    if(error){
+      error = [error];
+      this.handler.apply(this,[error].concat(data.slice(1)));
+    } else {
+      this.jobs.shift();
+      if(this.jobs.length > 0){
+        var job = this.jobs[0];
+        Procedure.Launcher(this,job.name,job.func,data.slice(1).concat(job.args));
+      } else {
+        this.handler.apply(this,data);
+      }
+    }
   };
 
 
 Procedure.Launcher = Launcher;
-function Launcher(context,job){
-  try {
-    job.func.apply(context,job.args);
-  } catch (error) {
-    context.handler(error);
-  }
+Procedure.Launcher.method = global.setImmediate;
+function Launcher(context,name,func,args){
+  return Procedure.Launcher.method(function(){
+    var catcher = context.done.bind(context,name);
+    catcher.result = catcher.bind(context,undefined);
+    try {
+      func.apply(context,args.concat([catcher]));
+    } catch (error) {
+      error.message = "Procedure error in '"+name+"' > "+error.name+" "+error.message;
+      error.name = "ProcedureError";
+      catcher(error);
+    }
+  });
 }
-
-
-
-
 
 
 /* ---------------------------- *
 
 // Launcher MOD for browser version.
-Procedure.Launcher = function jobLauncher(context,job){
-  setTimeout(function(){
-    try {
-      job.func.apply(context,job.args);
-    } catch (error) {
-      context.handler(error);
-    }
-  },Math.round(Math.random()*500));
-};
 
-/* ---------------------------- *
-
-
-// Test Procedure.Race & Procedure.Queue:
-
-function Myhand(a,b,c){
-  var job = this;
-  console.log(a,b,c);
-  job.done();
+Procedure.Launcher.method = function(task){
+    return setTimeout(task,Math.round(Math.random()*100));
 }
 
-var race = new Race();
-race.jobs.push({func:Myhand,args:['A1','A2','A3']});
-race.jobs.push({func:Myhand,args:['A4','A5','A6']});
-race.jobs.push({func:Myhand,args:['A7','A8','A9']});
-race.handler = function(error){
-  console.log("Finished!",error);
-};
-race.launch();
-
-var queue = new Queue();
-queue.jobs.push({func:Myhand,args:['B1','B2','B3']});
-queue.jobs.push({func:Myhand,args:['B4','B5','B6']});
-queue.jobs.push({func:Myhand,args:['B7','B8','B9']});
-queue.handler = function(error){
-  console.log("Finished!",error);
-};
-queue.launch();
-
 /* ---------------------------- *
 
-// Test Procedure:
-
-var procedure = new Procedure({counter:10}); // <- Initial data for share.
-
-// Add handlers inline...
-procedure
-.add(function(){
-  var job = this;
-  var name = "Race[A][1]";
-  console.log(name);
-  job.data.counter++;
-  job.done();
-})
-.add(function(){
-  var job = this;
-  var name = "Race[A][2]";
-  console.log(name);
-  job.data.counter++;
-  job.done();
-})
-.add(function(){
-  var job = this;
-  var name = "Race[A][3]";
-  console.log(name);
-  job.data.counter++;
-  job.done();
-})
-.add(function(){
-  var job = this;
-  var name = "Race[A][4]";
-  console.log(name);
-  job.data.counter++;
-  job.done();
-})
-.add(function(){
-  var job = this;
-  var name = "Race[A][5]";
-  console.log(name);
-  job.data.counter++;
-  job.done();
-})
-// Concurrent execution of pending jobs.
-.race(function(error){
-  if(!error){
-    console.log("Race[A] finished!");
-    console.log("Counter:",this.data.counter);
+function fileLoader(path,done){                        // File loader simulation.
+  console.log("Opening file:'"+path+"'...");
+  if(path==='./secundary2.key'){
+    //throw new Error("Ops!");                         // Error simulation
   }
-});
+  done.result({file:path});                            // Finalize task & return result
+}
+var shared = {};                                       // Shared closure
 
 
-// Add hanlers by reference with params [.add(handler,param1,param2,...)].
-var Myhand = function(name){
-  var job = this;
-  console.log(name);
-  job.data.counter++;
-  job.done();
-};
-
+var procedure = new Procedure();                       // Create procedure
 procedure
-.add(Myhand,"Queue[A][1]")
-.add(Myhand,"Queue[A][2]")
-.add(Myhand,"Queue[A][3]")
-.add(Myhand,"Queue[A][4]")
-.add(Myhand,"Queue[A][5]")
-// Queue execution (in order) of pending jobs.
-.queue(function(error){
-  if(!error){
-    console.log("Queue[A] finished!");
-    console.log("Counter:",this.data.counter);
-  }
+.add("Load primary",fileLoader,'./primary.key')        // Add new task
+.add("Load secundary",fileLoader,'./secundary.key')    // Add new task
+.add("Load shared",fileLoader,'./shared.key')          // Add new task
+.race()                                                // Resolve tasks in race mode
+.add(function(files,done){                             // Add new task
+    shared.files1 = files;
+    console.log(shared);
+    done();                                            // Finalize task
+})
+.queue()                                               // Resolve tasks in queue mode
+.add("Load primary",fileLoader,'./primary2.key')       // Add new task
+.add("Load secundary",fileLoader,'./secundary2.key')   // Add new task
+.add("Load shared",fileLoader,'./shared2.key')         // Add new task
+.race()                                                // Resolve tasks in race mode
+.add(function(files,done){                             // Add new task
+    shared.files2 = files;
+    console.log(shared);
+    done.result(shared);                                 // Finalize task & return result
+})
+.launch(function(errors){                              // Procedure controller.
+    if(errors){                                        // Array of Errors || undefined
+        console.log("END[ERROR]",arguments);
+    } else {
+        console.log("END[OK]",arguments);
+    }
 })
 ;
-
-
-
-// Add repetitive jobs - Race version
-for(var i=0;i<10;i++){ // <- Add repetitive jobs whith variable arguments.
-  procedure.add(Myhand,"Race[B]["+i+"]");
-}
-procedure.race(function(error){
-  if(!error){
-    console.log("Race[B] finished!");
-    console.log("Counter:",this.data.counter);
-  }
-});
-
-
-// Add repetitive jobs - Queue version
-for(var i=0;i<10;i++){
-  procedure.add(Myhand,"Queue[B]["+i+"]");
-}
-procedure.queue(function(error){
-  if(!error){
-    console.log("Queue[B] finished!");
-    console.log("Counter:",this.data.counter);
-  }
-});
-
-
-
-// Add independent jobs
-procedure.add(Myhand,"independent[X]");
-procedure.add(Myhand,"independent[Y]");
-procedure.add(Myhand,"independent[Z]");
-
-
-
-// And finally launch!
-procedure
-.launch(function(error){
-  if(error){ // Called by each error.
-    console.log("One Error:",error);
-  } else { // Called only if finished whitout errors.
-    console.log("All finished");
-    console.log("Counter:",this.data.counter);
-  }
-});
-
-
-/* ---------------------------- *
-
-var Myhand = function(name){
-  var job = this;
-  console.log(name);
-  job.data.counter++;
-  job.done();
-};
-
-// Errors management
-
-var procedure = new Procedure({counter:10}); // <- Initial data for share.
-
-procedure
-.add(Myhand,"Queue[C][1]")
-.add(Myhand,"Queue[C][2]")
-.add(function(){
-  var job = this;
-  console.log("Queue[C][3] Throw Error!...");
-  try {
-    var a = b;
-    job.done();
-  } catch(error) {
-    job.error(error);
-  }
-})
-.add(Myhand,"Queue[C][4]")
-.add(Myhand,"Queue[C][5]")
-.launch(function(error){
-  if(error){
-    console.log("One Error:",error);
-  } else {
-    console.log("All finished");
-    console.log("Counter:",this.data.counter);
-  }
-});
 
 /* ---------------------------- */
